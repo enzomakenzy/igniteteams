8b3ba0faa829cbfc0492e9503f7d035a14abf272
enzomakenzy <enzomqueiroz@gmail.com>
2025-04-28 17:15:22 -0300

    faltou um aff

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,9 @@
+import { Container, Content, Icon } from "./styles";
+
+import { Header } from "@components/Header";
+import { Button } from "@components/Button";
+import { Hightlight } from "@components/Highlight";
+
+export function NewGroup() {
+  
+}
